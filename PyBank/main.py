89a5c52ac79e9d5a9006f4379ab46ac160c61f2a
noahstettler <56611390+noahstettler@users.noreply.github.com>
2019-12-02 18:53:30 -0600

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,18 +7,20 @@ with open("budget_data.csv") as in_file:
     header = next(csv_reader)
     data = list(csv_reader)
     print("Financial Analysis")
+    f.write("Financial Analysis" + '\n')
 
     # Number of Months in the Period
     num_months = len(data)
     str_months = "Total number of months: " + str(num_months)
     print(str_months) 
-
+    f.write(str_months + '\n')
     # Total Profit\Loss
     total = 0
     for column in data:
       total += int(column[1])
     str_total = "Total Proft or Loss: $" + str(total)
     print(str_total)
+    f.write(str_total + '\n')
 
     # Average change in revenue 
     changes = []
@@ -31,8 +33,9 @@ with open("budget_data.csv") as in_file:
     net_change = sum(changes)
     avg_change = round(net_change/num_months,2)
     str_change = "Average Change in Revenue: $" + str(avg_change)
-    print(str_change)
-    
+    print(str_change + '\n')
+    f.write(str_change + '\n')
+
     # Return the Max increase and decrease along with its month
     max_increase = int(max(changes))
     max_decrease = int(min(changes))    
@@ -46,7 +49,8 @@ with open("budget_data.csv") as in_file:
     str_increase = "Greatest Increase in Revenue: " + month_i + ", $" + str(max_increase)
     str_decrease = "Greatest Decrease in Revenue: " + month_d + ", $" + str(max_decrease)
     print(str_increase)
+    f.write(str_increase + '\n')
     print(str_decrease)
+    f.write(str_decrease)
      
 f.close()
-  
